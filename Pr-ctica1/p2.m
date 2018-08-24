3e39787848ae34396c9871f39eb4fa0dcb15023f
sabaldaba <jor.sabal@hotmail.com>
2018-08-24 12:15:01 -0500

    Create p2.m

---
+++ Pr-ctica1/p2.m
@@ -0,0 +1,26 @@
+clear all
+close all
+clc
+
+Fs = 50;
+Ts = 1/Fs;
+Fs2 = 5000;
+Ts2= 1/Fs2;
+t = -1:Ts:1;
+t2 = -1:Ts2:1;
+n_max=50;
+
+x = sin(15*pi*t) + cos(20*pi*t);
+stem(t, x);
+hold on;
+
+fx = 0;
+
+for n = 1 : numel(t)
+    fx = fx + (x(n)*sinc((t2+1.02-((n)*Ts))/Ts));
+end
+
+plot(t2,fx);
+hold on;
+
+
